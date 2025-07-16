6414bb890b54866cac3825f37911315784fa0f54
nsn7890 <mninsb3@gmail.com>
2025-07-16 12:27:41 +0530

    FIX

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const API_KEY = "2b485952af285dced8fa720bec14ca4e";
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY; 
   
 
   const fetchWeather = async () => {
